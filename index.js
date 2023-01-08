2ee7899816feaeebf1f2236b9b966474fc506b4f
Sola-Aremu 'Pelumi <solaaremu.pelumi@gmail.com>
2023-01-08 15:34:38 +0000

    updating to be mobile friendly

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ input.addEventListener("keypress", function(event){
 
 function generateNumeronym() {
     let word = input.value
-    console.log(word)
     input.value = ''
     let first = word[0]
     let last = word[word.length-1]
     let result = first + (word.length - 2) + last
+    result = result.toLowerCase()
     result_grid.innerHTML = result
     btn.disabled = true
 }
 
 function stateHandle(event) {
-    console.log("firing")
     if (input.value === '') {
         btn.disabled = true;
         result_grid.innerHTML = ""
